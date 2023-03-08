1bad1ccf33ec283bb2eb21737dc35a15cae92bf0
Jussier da Costa Almeida <jussier.almeida@saude.govrn>
2023-03-08 16:14:10 -0300

    meu primeiro commit

---
+++ organizacao.js
@@ -0,0 +1,122 @@
+/*let nome= "caneta";
+let quantidade =10;
+let preco = 6.4;
+let imposto = 1.4;
+let precoFinal = preco + imposto;
+
+
+console.log(nome);
+console.log("o preço final é:" + precoFinal);
+
+let raio =10;
+const PI = 3.14;
+let circunferencia = PI * raio * raio;
+let area = Math.PI *raio * raio;
+console.log('a circunferencia é: '+ circunferencia + ' m2');
+console.log('a área é: '+ area + '  m2');
+
+//troca de valores de variavel
+let a =20;
+let b =60;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log('o valor de a é: '+ a);
+console.log('o valor de b é: '+ b);
+
+const valor = 1000;
+const valores = [5.5, 2]
+console.log(typeof valor); typo
+console.log(typeof valor);
+console.log(typeof valores);
+const valores = [5.5, 2, 45];arrai matriz
+
+console.log(valores.length); quantidade*/
+
+/*const valor = 1000;
+const valores = [5.5, 2, valor];
+valores.push({id:5}, false, null, 'texto'); função para add elementos; id é objeto 
+console.log(valores);
+console.log(valores.pop());
+delete valores[0];
+console.log(valores);
+console.log('valor: '+valores[2]);
+console.log(typeof valores);
+console.log('qtd: ' +valores.length);*/
+
+/*const nome=[];array
+const prod1={};objeto
+nome.push(10 ,30);
+console.log(nome);
+delete nome[1];
+console.log(nome);
+prod1.nome = 'celular ultra mega';evitar atributoa com espaço
+prod1.preco = 51.003;
+prod1['celular bombastico'] = 0.50;
+console.log(prod1);
+const prod2 = {
+    nome: 'Camisa Polo',
+    preço: 52.006,
+    obj:{
+        blabla:1,
+        obj:{
+            blabla:2
+        }
+    }
+}
+console.log(prod2);
+
+função
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+imprimirSoma(5, 6)
+função
+function soma(a, b =1){
+    return a * b
+}
+console.log(soma(5, 6))
+console.log(soma(5))
+
+
+const imprimirSoma = function(a, b){
+    console.log(a + b)
+}
+imprimirSoma(4 , 5)
+
+const subtracao = (a , b) => a - b
+
+console.log(subtracao(7, 9))*/
+
+const imprimir2 = nome => console.log(nome)
+imprimir2('Legal!!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
